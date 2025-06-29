10bdb8274f40e4bc45212e2c8fc4aaffe44f441f
andreibalcan <andreiflorinbalcan@gmail.com>
2025-06-29 09:08:33 +0100

    new build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideHttpClient(),
+        {
+            provide: LocationStrategy, 
+            useClass: HashLocationStrategy
+        }
     ],
 };
